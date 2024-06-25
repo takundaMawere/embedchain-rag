6c0c1b1792f3aad339393f10b47c8568d96c762c
takundaMawere <taku15mawere@gmail.com>
2024-06-25 15:34:36 +0200

    K

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import dropbox
 import streamlit as st
-import poppler
 import os
 import time
-from pdf2image import convert_from_path
-import pytesseract
 from embedchain import App
 
 with st.sidebar:
@@ -68,50 +65,50 @@ with st.sidebar:
             # List and download PDFs
 
             # section2
-            def list_and_download_pdfs(dbx, folder_path):
-                pdf_files = []
-                result = dbx.files_list_folder(folder_path)
-                while result.entries:
-                    for entry in result.entries:
-                        if isinstance(entry, dropbox.files.FileMetadata) and entry.name.endswith('.pdf'):
-
-                            local_file_path = os.path.join(local_folder_path, entry.name)
-                            if not os.path.exists(local_file_path):
-                                pdf_files.append(entry.name)
-                            with open(local_file_path, "wb") as f:
-                                metadata, res = dbx.files_download(entry.path_lower)
-                                f.write(res.content)
-                    if result.has_more:
-                        result = dbx.files_list_folder_continue(result.cursor)
-                    else:
-                        break
-                return pdf_files
-
-
-            pdf_files = list_and_download_pdfs(dbx, dropbox_folder_path)
-            print(f"Downloaded PDFs: {pdf_files}")
-
-
-            # Convert PDFs to text using OCR
-            def pdf_to_text(pdf_path):
-                pages = convert_from_path(pdf_path, 300)
-                texts = [pytesseract.image_to_string(page) for page in pages]
-                return "\n".join(texts)
-
-
-            pdf_texts = []
-            for pdf_file in pdf_files:
-                local_pdf_path = os.path.join(local_folder_path, pdf_file)
-                text = pdf_to_text(local_pdf_path)
-                print("Added")
-                pdf_texts.append((pdf_file, text))
-
-            # section 3
+            # def list_and_download_pdfs(dbx, folder_path):
+            #     pdf_files = []
+            #     result = dbx.files_list_folder(folder_path)
+            #     while result.entries:
+            #         for entry in result.entries:
+            #             if isinstance(entry, dropbox.files.FileMetadata) and entry.name.endswith('.pdf'):
+            #
+            #                 local_file_path = os.path.join(local_folder_path, entry.name)
+            #                 if not os.path.exists(local_file_path):
+            #                     pdf_files.append(entry.name)
+            #                 with open(local_file_path, "wb") as f:
+            #                     metadata, res = dbx.files_download(entry.path_lower)
+            #                     f.write(res.content)
+            #         if result.has_more:
+            #             result = dbx.files_list_folder_continue(result.cursor)
+            #         else:
+            #             break
+            #     return pdf_files
+            #
+            #
+            # pdf_files = list_and_download_pdfs(dbx, dropbox_folder_path)
+            # print(f"Downloaded PDFs: {pdf_files}")
+            #
+            #
+            # # Convert PDFs to text using OCR
+            # def pdf_to_text(pdf_path):
+            #     pages = convert_from_path(pdf_path, 300)
+            #     texts = [pytesseract.image_to_string(page) for page in pages]
+            #     return "\n".join(texts)
+            #
+            #
+            # pdf_texts = []
+            # for pdf_file in pdf_files:
+            #     local_pdf_path = os.path.join(local_folder_path, pdf_file)
+            #     text = pdf_to_text(local_pdf_path)
+            #     print("Added")
+            #     pdf_texts.append((pdf_file, text))
+            #
+            # # section 3
             app = App.from_config(config=config)
             # Add extracted texts to EmbedChain
-            for pdf_file, text in pdf_texts:
-                print("something is being added \n")
-                app.add(text, data_type='text')
+            # for pdf_file, text in pdf_texts:
+            #     print("something is being added \n")
+            #     app.add(text, data_type='text')
 
             app.add("/gen2", data_type="dropbox")
             # Query the embedded texts
